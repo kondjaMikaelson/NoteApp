7bf3fa5e747a6f85b7352e6d4de3a68ea065134e
kondjaMikaelson <ngendinakondja@gmail.com>
2022-06-30 08:38:49 +0200

    unused imports

--- app/src/main/java/com/kondja/noteapp/MainActivity.java
+++ app/src/main/java/com/kondja/noteapp/MainActivity.java
@@ -1,13 +1,8 @@
 package com.kondja.noteapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.kondja.noteapp.Database.NoteDao;
-import com.kondja.noteapp.Database.NoteDatabase;
-
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
