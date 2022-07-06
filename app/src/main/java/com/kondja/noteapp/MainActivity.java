dddb8791ac47ec8637ef4ecc5dd1f37aac159b73
kondjaMikaelson <ngendinakondja@gmail.com>
2022-07-06 16:18:25 +0200

    added recycler view to main activity

--- app/src/main/java/com/kondja/noteapp/MainActivity.java
+++ app/src/main/java/com/kondja/noteapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.kondja.noteapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final NoteListAdapter adapter = new NoteListAdapter(new NoteListAdapter.NoteDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
