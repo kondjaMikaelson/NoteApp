ed9bf869f874961d26bc5dfe30dd658df66de541
kondjaMikaelson <ngendinakondja@gmail.com>
2022-07-11 16:25:27 +0200

    fixed NewNoteActivity.java with minor issues.

--- app/src/main/java/com/kondja/noteapp/MainActivity.java
+++ app/src/main/java/com/kondja/noteapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.kondja.noteapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.kondja.noteapp.Database.NoteViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +24,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final NoteListAdapter adapter = new NoteListAdapter(new NoteListAdapter.NoteDiff());
         recyclerView.setAdapter(adapter);
@@ -31,6 +33,22 @@ public class MainActivity extends AppCompatActivity {
         noteViewModel.getAllNotes().observe(this, notes -> {
             adapter.submitList(notes);
         });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, NewNoteActivity.class);
+            startActivityForResult(intent, NEW_NOTE_ACTIVITY_REQUEST_CODE);
+        });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == NEW_NOTE_ACTIVITY_REQUEST_CODE){
+            Note note = new Note(data.getStringExtra((NewNoteActivity.EXTRA_REPLY)),"");
+            noteViewModel.insert(note);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Not Saved", Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/kondja/noteapp/NewNoteActivity.java
+++ app/src/main/java/com/kondja/noteapp/NewNoteActivity.java
@@ -1,16 +1,35 @@
 package com.kondja.noteapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewNoteActivity extends AppCompatActivity {
 
+    public static final String EXTRA_REPLY = "com.kondja.noteapp.REPLY";
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+        editText = findViewById(R.id.edit_word);
 
-
+        final Button button = findViewById(R.id.button_save);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            if(TextUtils.isEmpty(editText.getText())){
+                setResult(RESULT_CANCELED, intent);
+            }else{
+                String note = editText.getText().toString();
+                intent.putExtra(EXTRA_REPLY, note);
+                setResult(RESULT_OK,intent);
+            }
+            finish();
+        });
     }
 }
